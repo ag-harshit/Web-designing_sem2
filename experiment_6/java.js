ed8f242545c15be4249b651711e285559273dc4a
HARSHIT <agharshit006@gmail.com>
2026-03-17 09:45:22 +0530

    experiment_6

---
+++ experiment_6/java.js
@@ -0,0 +1,79 @@
+function changeHeading(){
+    let text=document.getElementById("textInput").value;
+    document.getElementById("heading").innerHTML=text;
+}
+
+function changeBackground(){
+    document.body.style.backgroundColor="lightyellow";
+}
+
+function increaseFont(){
+    document.getElementById("heading").style.fontSize="50px";
+}
+
+function toggleParagraph(){
+    let p=document.getElementById("paragraph");
+
+    if(p.style.display==="none"){
+        p.style.display="block";
+    }
+    else{
+        p.style.display="none";
+    }
+}
+
+function resetPage(){
+    location.reload();
+}function changeHeading(){
+    let text=document.getElementById("textInput").value;
+    document.getElementById("heading").innerHTML=text;
+}
+
+function changeBackground(){
+    document.body.style.backgroundColor="lightyellow";
+}
+
+function increaseFont(){
+    document.getElementById("heading").style.fontSize="50px";
+}
+
+function toggleParagraph(){
+    let p=document.getElementById("paragraph");
+
+    if(p.style.display==="none"){
+        p.style.display="block";
+    }
+    else{
+        p.style.display="none";
+    }
+}
+
+function resetPage(){
+    location.reload();
+}function changeHeading(){
+    let text=document.getElementById("textInput").value;
+    document.getElementById("heading").innerHTML=text;
+}
+
+function changeBackground(){
+    document.body.style.backgroundColor="lightyellow";
+}
+
+function increaseFont(){
+    document.getElementById("heading").style.fontSize="50px";
+}
+
+function toggleParagraph(){
+    let p=document.getElementById("paragraph");
+
+    if(p.style.display==="none"){
+        p.style.display="block";
+    }
+    else{
+        p.style.display="none";
+    }
+}
+
+function resetPage(){
+    location.reload();
+}
